cf887ad0901235cb921eb090c634f24ba1fbae4e
Niyonshuti Adelite <shemani2011@gmail.com>
2023-08-19 15:40:36 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog'
 ]
 
@@ -121,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
