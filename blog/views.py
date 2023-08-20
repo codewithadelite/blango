92f395406a6e5c2fc2451e0919f34410e40c631f
Niyonshuti Adelite <shemani2011@gmail.com>
2023-08-20 11:46:52 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render,redirect, get_object_or_404
 
+
 from blog.models import Post
 from blog.forms import CommentForm
 
@@ -13,7 +14,6 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
-
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
