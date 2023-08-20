40b3829a3211ef2dcc17ae99eda28be3366e426c
Niyonshuti Adelite <shemani2011@gmail.com>
2023-08-20 08:50:02 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 import blog.views
 
+
 urlpatterns = [
     path("", blog.views.index),
         path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
