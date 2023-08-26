7f50207109d31e286eb17c24c4bd35b9847051e0
Niyonshuti Adelite <shemani2011@gmail.com>
2023-08-26 09:26:19 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -24,6 +24,7 @@ from blango_auth.forms import BlangoRegistrationForm
 
 
 urlpatterns = [
+    path("api/v1/", include("blog.api_urls")),
     path("", blog.views.index),
         path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path('admin/', admin.site.urls),
